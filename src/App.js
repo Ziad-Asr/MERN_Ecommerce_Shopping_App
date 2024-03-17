ec1412a0a7bedfee7221a7a00bb5b3110e08e321
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-03-17 02:20:25 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Home from "./pages/Home/Home";
+// import Product from "./pages/Product/Product";
+// import ProductList from "./pages/ProductList/ProductList";
+// import Register from "./pages/Register/Register";
+// import Login from "./pages/Login/Login";
+// import Cart from "./pages/Cart/Cart";
+
 const App = () => {
-  return <div className="App"></div>;
+  return <Home />;
 };
 
 export default App;
