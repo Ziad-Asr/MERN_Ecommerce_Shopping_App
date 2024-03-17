b3e47013da9e4ceabc8f51fb066d412454188c0e
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-03-17 05:23:19 +0200

    update

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,27 @@
-import "./Product.scss";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
-const Product = () => {
-  return <div>Product</div>;
+import styles from "./Product.module.scss";
+
+const Product = ({ item }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.circle}></div>
+      <img className={styles.image} src={item.img} alt="Product" />
+      <div className={styles.info}>
+        <div className={styles.icon}>
+          <ShoppingCartOutlinedIcon />
+        </div>
+        <div className={styles.icon}>
+          <SearchOutlinedIcon />
+        </div>
+        <div className={styles.icon}>
+          <FavoriteBorderOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,7 +1,76 @@
-import "./Product.scss";
+import Announcement from "../Announcement/Announcement";
+import Navbar from "../Navbar/Navbar";
+import Newsletter from "../Newsletter/Newsletter";
+import Footer from "../Footer/Footer";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
+
+import styles from "./Product.module.scss";
 
 const Product = () => {
-  return <div>Product</div>;
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <Announcement />
+      <div className={styles.wrapper}>
+        <div className={styles.imgContainer}>
+          <img
+            className={styles.image}
+            src="https://i.ibb.co/S6qMxwr/jean.jpg"
+            alt="Denim Jumpsuit"
+          />
+        </div>
+        <div className={styles.infoContainer}>
+          <h1 className={styles.title}>Denim Jumpsuit</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            venenatis, dolor in finibus malesuada, lectus ipsum porta nunc, at
+            iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
+            tristique tortor pretium ut. Curabitur elit justo, consequat id
+            condimentum ac, volutpat ornare.
+          </p>
+          <span className={styles.price}>$ 20</span>
+          <div className={styles.filterContainer}>
+            <div className={styles.filter}>
+              <span className={styles.filterTitle}>Color</span>
+              <div
+                className={styles.filterColor}
+                style={{ backgroundColor: "black" }}
+              ></div>
+              <div
+                className={styles.filterColor}
+                style={{ backgroundColor: "darkblue" }}
+              ></div>
+              <div
+                className={styles.filterColor}
+                style={{ backgroundColor: "gray" }}
+              ></div>
+            </div>
+            <div className={styles.filter}>
+              <span className={styles.filterTitle}>Size</span>
+              <select className={styles.filterSize}>
+                <option>XS</option>
+                <option>S</option>
+                <option>M</option>
+                <option>L</option>
+                <option>XL</option>
+              </select>
+            </div>
+          </div>
+          <div className={styles.addContainer}>
+            <div className={styles.amountContainer}>
+              <RemoveIcon className={styles.icon} />
+              <span className={styles.amount}>1</span>
+              <AddIcon className={styles.icon} />
+            </div>
+            <button className={styles.button}>ADD TO CART</button>
+          </div>
+        </div>
+      </div>
+      <Newsletter />
+      <Footer />
+    </div>
+  );
 };
 
 export default Product;
