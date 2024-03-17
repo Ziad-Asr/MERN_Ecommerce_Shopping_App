efe8ab4cbcfaaceda99492bf2e2e69415810f52f
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-03-17 05:40:27 +0200

    update

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,7 +1,56 @@
-import "./ProductList.scss";
+import Announcement from "../../components/Announcement/Announcement";
+import Navbar from "../../components/Navbar/Navbar";
+import Products from "../../components/Products/Products";
+import Newsletter from "../../components/Newsletter/Newsletter";
+import Footer from "../../components/Footer/Footer";
+
+import styles from "./ProductList.module.scss";
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <Announcement />
+      <h1 className={styles.title}>Dresses</h1>
+      <div className={styles.filterContainer}>
+        <div className={styles.filter}>
+          <span className={styles.filterText}>Filter Products:</span>
+          <select className={styles.select}>
+            <option disabled selected>
+              Color
+            </option>
+            <option>White</option>
+            <option>Black</option>
+            <option>Red</option>
+            <option>Blue</option>
+            <option>Yellow</option>
+            <option>Green</option>
+          </select>
+          <select className={styles.select}>
+            <option disabled selected>
+              Size
+            </option>
+            <option>XS</option>
+            <option>S</option>
+            <option>M</option>
+            <option>L</option>
+            <option>XL</option>
+          </select>
+        </div>
+        <div className={styles.filter}>
+          <span className={styles.filterText}>Sort Products:</span>
+          <select className={styles.select}>
+            <option selected>Newest</option>
+            <option>Price (asc)</option>
+            <option>Price (desc)</option>
+          </select>
+        </div>
+      </div>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
 };
 
 export default ProductList;
