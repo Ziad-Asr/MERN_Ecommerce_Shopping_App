ff307282db33a89ab8107145d845c272429929fb
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-03-17 03:30:05 +0200

    update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,36 @@
-import "./Navbar.scss";
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
+import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.left}>
+          <span className={styles.language}>EN</span>
+          <div className={styles.searchContainer}>
+            <input className={styles.input} placeholder="Search" />
+            <SearchIcon style={{ color: "gray", fontSize: 16 }} />
+          </div>
+        </div>
+        <div className={styles.center}>
+          <h1 className={styles.logo}>LAMA.</h1>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.menuItem}>REGISTER</div>
+          <div className={styles.menuItem}>SIGN IN</div>
+          <div className={styles.menuItem}>
+            <div className={styles.topbarIconContainer}>
+              <ShoppingCartOutlinedIcon />
+              <span className={styles.topIconBadge}>1</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
