a959624b0edffcbf81796589ff54f1f8a5924661
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-03-18 05:03:59 +0200

    update

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,5 +1,32 @@
 const router = require("express").Router();
 
 const orderController = require("../controllers/order");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("../utils/verifyToken");
+
+//GET ALL ORDERS
+router.get("/", verifyTokenAndAdmin, orderController.getAllOrders);
+
+//GET USER ORDERS
+router.get(
+  "/find/:userId",
+  verifyTokenAndAuthorization,
+  orderController.getUserOrders
+);
+
+// GET MONTHLY INCOME
+router.get("/income", verifyTokenAndAdmin, orderController.getMonthlyIncome);
+
+//CREATE ORDER
+router.post("/", verifyToken, orderController.createOrder);
+
+//UPDATE ORDER
+router.put("/:id", verifyTokenAndAdmin, orderController.updateOrder);
+
+//DELETE ORDER
+router.delete("/:id", verifyTokenAndAdmin, orderController.deleteOrder);
 
 module.exports = router;
--- api/routes/product.js
+++ api/routes/product.js
@@ -1,5 +1,26 @@
 const router = require("express").Router();
 
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("../utils/verifyToken");
+
 const productController = require("../controllers/product");
+
+//GET ALL PRODUCTS
+router.get("/", productController.getAllProducts);
+
+//GET PRODUCT BY ID
+router.get("/find/:id", productController.getProduct);
+
+//CREATE PRODUCT
+router.post("/", verifyTokenAndAdmin, productController.createProduct);
+
+//UPDATE PRODUCT
+router.put("/:id", verifyTokenAndAdmin, productController.updateProduct);
+
+//DELETE PRODUCT
+router.delete("/:id", verifyTokenAndAdmin, productController.deleteProduct);
 
 module.exports = router;
